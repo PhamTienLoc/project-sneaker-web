97ab140eb7ba494cc5d70b301313cbfbece08dad
PhamTienLoc <21130429@st.hcmuaf.edu.vn>
2025-05-30 09:00:19 +0700

    init backend project

---
+++ backend/sneaker-shop-backend/src/main/java/com/sneakershop/SneakerShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sneakershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerShopBackendApplication.class, args);
+	}
+
+}
